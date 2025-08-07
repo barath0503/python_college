b5e82a7aca3bf32a5cdea4de407b8d6c96f515c7
barath0503 <barath122112@gmail.com>
2025-08-07 19:52:50 +0530

    progran updated

--- dictionry.py
+++ dictionry.py
@@ -1,2 +1,22 @@
-field=("name","age","city")
-data=("Alice",21,"Delhi")
+def q1():
+    field=("name","age","city")
+    data=("Alice",21,"Delhi")
+    print(dict(zip(field,data)))
+def q2():
+    data = {"a": [1], "b": [1, 2, 3], "c": [1, 2]}
+    def get_length(item):
+        return len(item[1])
+    sorted_dict = dict(sorted(data.items(), key=get_length))
+    print(sorted_dict)
+def q3():
+    data=[(1, 2), (2, 3), (1, 2), (2, 3), (2, 3)]
+    d={}
+    for i in data:
+        if i in d:
+            d[i]+=1
+        else:
+            d[i]=1
+    print(d)
+q3()
+
+
